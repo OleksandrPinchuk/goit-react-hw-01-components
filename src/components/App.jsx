727669004f5ed420fb39610fc092b4840e1a4d8a
OleksandrPinchuk <hohhua@gmail.com>
2023-07-06 02:28:27 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from './profile/Profile';
+import user from '../data/user.json';
 
-console.log(App);
+
+export const App = () => {
+    return (
+        <div>
+            <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+
+            />
+        </div>
+    );
+};
